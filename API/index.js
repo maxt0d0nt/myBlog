d7b4cc08506354c68b4b078e9373e2f2c9255635
maxt0d0nt <maxbox21@hotmail.com>
2022-10-21 12:38:46 +0200

    register & login mongoDB

--- API/index.js
+++ API/index.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth")
+
 
 dotenv.config();
+app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL)
-.then(console.log("conectd"))
+mongoose.connect(process.env.MONGO_URL,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+.then(console.log("conectado a la BBDD"))
 .catch((err) => console.log(err));
+
+app.use("/api/auth", authRoute)
 
 app.listen("5000", () =>{
     console.log("server run")
